375eb330f77d650c990d35fd113521af96d5a6bb
Gaya Kessler <gaya@theclevernode.com>
2020-04-28 10:41:20 +0200

    room create join page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import "~@blueprintjs/core/lib/css/blueprint.css";
-import "~@blueprintjs/icons/lib/css/blueprint-icons.css";
+
+import 'normalize.css/normalize.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+
+import { Spinner } from '@blueprintjs/core';
 
 import useApp from '../../stores/app/useApp';
 
+import './App.css';
 import Room from './Room';
 
 const App: React.FunctionComponent = () => {
-  const gameState = useApp();
+  const [gameState] = useApp();
+
+  const { isActive, activeRoom } = gameState;
+
+  const isInRoom = activeRoom !== '';
+
+  if (!isActive) {
+    return (
+      <div className="App App--loading">
+        <Spinner size={50} />
+      </div>
+    );
+  }
 
   return (
-    <Room />
+    <div className="App">
+      {!isInRoom && <Room />}
+    </div>
   );
 }
 
--- src/components/App/Room.tsx
+++ src/components/App/Room.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import {
+  Button,
+  Card,
+  Divider,
+  Elevation,
+  FormGroup,
+  H5,
+  InputGroup,
+  Intent
+} from '@blueprintjs/core';
+
+import './Room.css';
 
 const Room: React.FunctionComponent = () => {
-  return <div>Room</div>;
+  return (
+    <div className="Room">
+      <Card elevation={Elevation.ONE}>
+        <H5>
+          Create room
+        </H5>
+        <p>
+          Want to host a room for your friend to come and play?
+        </p>
+        <Button rightIcon="insert" intent={Intent.SUCCESS} text="Create new Room" />
+
+        <Divider className="Room__divider" />
+
+        <H5>
+          Join existing room
+        </H5>
+        <p>
+          Someone already opened a room you want to join?
+        </p>
+
+        <form onSubmit={(e) => { e.preventDefault(); }}>
+          <FormGroup
+            label={"Room ID"}
+            labelFor="room-id"
+          >
+            <InputGroup id="room-id" placeholder="eg: PPBqWA9" />
+          </FormGroup>
+
+          <Button icon="log-in" text="Join room" />
+        </form>
+      </Card>
+    </div>
+  );
 };
 
 export default Room;
