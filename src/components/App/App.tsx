cf3702f43bdad42d635e0591e11f894ff561d8fe
Gaya Kessler <gaya@theclevernode.com>
2020-04-28 09:52:20 +0200

    add ui framework

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import "~@blueprintjs/core/lib/css/blueprint.css";
+import "~@blueprintjs/icons/lib/css/blueprint-icons.css";
 
 import useApp from '../../stores/app/useApp';
 
-function App(): React.ReactElement {
+import Room from './Room';
+
+const App: React.FunctionComponent = () => {
   const gameState = useApp();
 
   return (
-    <div>Test</div>
+    <Room />
   );
 }
 
---
+++ src/components/App/Room.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Room: React.FunctionComponent = () => {
+  return <div>Room</div>;
+};
+
+export default Room;
