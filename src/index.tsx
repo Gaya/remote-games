cf2f72ba606d3fc03a6fa2a76726d23e27454683
Gaya Kessler <gaya@theclevernode.com>
2020-05-04 13:57:22 +0200

    disable strictmode

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import { render } from 'react-dom';
 import App from './components/App/App';
 
 render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root'),
 );
