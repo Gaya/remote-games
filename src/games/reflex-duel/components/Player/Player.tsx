6783aa3263fa190a0c33ef4c3af4201a13378e0d
Gaya Kessler <gaya@theclevernode.com>
2020-05-12 13:27:19 +0200

    create start screen

--- src/games/reflex-duel/components/Player/Player.tsx
+++ src/games/reflex-duel/components/Player/Player.tsx
@@ -9,6 +9,7 @@ interface PlayerProps {
   character: Character;
   pose: Pose;
   scale?: number;
+  flipped?: boolean;
 }
 
 interface PlayerStyle {
@@ -17,9 +18,10 @@ interface PlayerStyle {
   backgroundPositionY: number;
   width: number;
   height: number;
+  transform?: string;
 }
 
-function styleForPose(sprite: PlayerSprite, pose: Pose, scale = 1): PlayerStyle {
+function styleForPose(sprite: PlayerSprite, pose: Pose, scale = 1, flipped = false): PlayerStyle {
   const box = sprite[pose];
 
   return {
@@ -28,16 +30,19 @@ function styleForPose(sprite: PlayerSprite, pose: Pose, scale = 1): PlayerStyle 
     backgroundPositionY: (box.y1 * scale) * -1,
     width: (box.x2 * scale) - (box.x1 * scale),
     height: (box.y2 * scale) - (box.y1 * scale),
+    transform: flipped ? 'scaleX(-1)' : undefined,
   };
 }
 
-const Player: React.FC<PlayerProps> = ({ character, pose, scale = 2 }) => {
+const Player: React.FC<PlayerProps> = ({
+  character, pose, scale = 2, flipped = false,
+}) => {
   const sprite = sprites.get(character);
 
   if (!sprite) return null;
 
   return (
-    <div className="ReflexDuel__Player" style={{ ...styleForPose(sprite, pose, scale) }} />
+    <div className="ReflexDuel__Player" style={{ ...styleForPose(sprite, pose, scale, flipped) }} />
   );
 };
 
--- src/games/reflex-duel/screens/Start/Start.tsx
+++ src/games/reflex-duel/screens/Start/Start.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 
 import Player from '../../components/Player/Player';
 import { Character, Pose } from '../../types';
+import background from '../../assets/bg.png';
 
 import './Start.css';
 
@@ -24,8 +25,39 @@ const Start: React.FC<StartProps> = ({ waitingForPlayers }) => {
   }, [count, waitingForPlayers]);
 
   return (
-    <div className="ReflexDuel__Start">
+    <div className="ReflexDuel__Start" style={{ backgroundImage: `url(${background})` }}>
       <h2 className="ReflexDuel__Start__Title">Reflex Duel</h2>
+
+      <div className="ReflexDuel__PickFighter">
+        <p>Pick a Fighter</p>
+        <div className="ReflexDuel__PickFighter__Items">
+          <button
+            type="button"
+            className={
+              classNames(
+                'ReflexDuel__PickFighter__Item',
+                { 'ReflexDuel__PickFighter__Item--active': fighter === Character.A },
+              )
+            }
+            onClick={(): void => setFighter(Character.A)}
+          >
+            <Player character={Character.A} pose={Pose.IDLE} />
+          </button>
+          <button
+            type="button"
+            className={
+              classNames(
+                'ReflexDuel__PickFighter__Item',
+                { 'ReflexDuel__PickFighter__Item--active': fighter === Character.B },
+              )
+            }
+            onClick={(): void => setFighter(Character.B)}
+          >
+            <Player character={Character.B} pose={Pose.IDLE} />
+          </button>
+        </div>
+      </div>
+
       {waitingForPlayers && (
         <p className="ReflexDuel__Start__CountDown">Waiting for others to join...</p>
       )}
@@ -38,32 +70,6 @@ const Start: React.FC<StartProps> = ({ waitingForPlayers }) => {
           seconds
         </p>
       )}
-      <div className="ReflexDuel__PickFighter">
-        <button
-          type="button"
-          className={
-            classNames(
-              'ReflexDuel__PickFighter__Item',
-              { 'ReflexDuel__PickFighter__Item--active': fighter === Character.A },
-            )
-          }
-          onClick={(): void => setFighter(Character.A)}
-        >
-          <Player character={Character.A} pose={Pose.IDLE} />
-        </button>
-        <button
-          type="button"
-          className={
-            classNames(
-              'ReflexDuel__PickFighter__Item',
-              { 'ReflexDuel__PickFighter__Item--active': fighter === Character.B },
-            )
-          }
-          onClick={(): void => setFighter(Character.B)}
-        >
-          <Player character={Character.B} pose={Pose.IDLE} />
-        </button>
-      </div>
     </div>
   );
 };
