a727b164c91eac843d0947764a01f09d3bf5e538
Gaya Kessler <gaya@theclevernode.com>
2020-05-20 08:18:10 +0200

    use express as wss server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,22 @@
 import WebSocket from 'ws';
 import serveStatic from 'serve-static';
 import express from 'express';
-
+import * as http from 'http';
 import * as path from 'path';
+
 import { WSActionTypes } from '../ws/actions';
 
 import { createUser } from './entities/user';
 import { log } from './logging';
 import handleMessage from './controllers';
 
-const wss = new WebSocket.Server({ port: parseInt(process.env.WS_PORT || '4000', 10) });
+const serve = serveStatic(path.join(__dirname, '../../build'));
+const app = express();
+
+app.use(serve);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   // create user for session
@@ -43,11 +50,6 @@ wss.on('connection', (ws) => {
   });
 });
 
-const serve = serveStatic(path.join(__dirname, '../../build'));
-const app = express();
-
-app.use(serve);
-
-app.listen(parseInt(process.env.PORT || '5000', 10));
-
-log('Server started');
+server.listen(parseInt(process.env.PORT || '5000', 10), () => {
+  log('Server started');
+});
